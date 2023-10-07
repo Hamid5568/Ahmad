06ea6596e4e1e88f72fbb66271f4658c02a7167d
Hamid5568 <140995629+Hamid5568@users.noreply.github.com>
2023-10-08 00:20:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,87 @@
 const studentList = document.querySelector('#student-list tbody');
 const searchBar = document.querySelector('#search-bar');
 
-searchBar.addEventListener('keyup', function() {
-  const searchTerm = this.value.toLowerCase();
+// Get all students from the backend
+async function getStudents() {
+  const response = await axios.get('/students');
 
-  const students = studentList.querySelectorAll('tr');
+  // If the response is successful, return the students
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    // Handle the error
+    console.log(response.statusText);
+    return [];
+  }
+}
+
+// Display the students in the table
+function displayStudents(students) {
+  studentList.innerHTML = '';
 
   for (let i = 0; i < students.length; i++) {
     const student = students[i];
-    const name = student.querySelector('td:nth-child(1)').textContent.toLowerCase();
-    const rollNumber = student.querySelector('td:nth-child(2)').textContent.toLowerCase();
+    const tableRow = document.createElement('tr');
 
-    if (name.includes(searchTerm) || rollNumber.includes(searchTerm)) {
-      student.style.display = '';
-    } else {
-      student.style.display = 'none';
-    }
+    const nameCell = document.createElement('td');
+    nameCell.textContent = student.name;
+    tableRow.appendChild(nameCell);
+
+    const rollNumberCell = document.createElement('td');
+    rollNumberCell.textContent = student.rollNumber;
+    tableRow.appendChild(rollNumberCell);
+
+    const classCell = document.createElement('td');
+    classCell.textContent = student.class;
+    tableRow.appendChild(classCell);
+
+    const obtainedMarksCell = document.createElement('td');
+    obtainedMarksCell.textContent = student.obtainedMarks;
+    tableRow.appendChild(obtainedMarksCell);
+
+    const phoneNumberCell = document.createElement('td');
+    phoneNumberCell.textContent = student.phoneNumber;
+    tableRow.appendChild(phoneNumberCell);
+
+    studentList.appendChild(tableRow);
   }
-});
+}
 
-// This code will connect the frontend to the backend using Node.js
+// Search for students by name or roll number
+async function searchStudents(searchTerm) {
+  const students = await getStudents();
 
-const axios = require('axios');
-
-// Add a new student
-async function addStudent(name, rollNumber, class, obtainedMarks, phoneNumber) {
-  const response = await axios.post('/add-student', {
-    name,
-    rollNumber,
-    class,
-    obtainedMarks,
-    phoneNumber,
+  // Filter the students by name or roll number
+  const filteredStudents = students.filter((student) => {
+    return student.name.toLowerCase().includes(searchTerm.toLowerCase()) || student.rollNumber.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
-  // If the response is successful, add the student to the list
-  if (response
+  // Display the filtered students
+  displayStudents(filteredStudents);
+}
+
+// Handle errors
+function handleError(error) {
+  console.log(error);
+
+  // Display an error message to the user
+  alert('An error occurred. Please try again later.');
+}
+
+// Get all students when the page loads
+getStudents()
+  .then((students) => {
+    displayStudents(students);
+  })
+  .catch(handleError);
+
+// Search for students when the user types in the search bar
+searchBar.addEventListener('keyup', async function() {
+  const searchTerm = this.value;
+
+  try {
+    await searchStudents(searchTerm);
+  } catch (error) {
+    handleError(error);
+  }
+});
