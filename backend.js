69ba407407f1658583e745faf01a4a4953f1f5a3
Hamid5568 <140995629+Hamid5568@users.noreply.github.com>
2023-10-08 00:24:49 +0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -6,42 +6,92 @@ const app = express();
 // Parse JSON requests
 app.use(bodyParser.json());
 
-// Add a new student
-app.post('/add-student', async (req, res) => {
-  const name = req.body.name;
-  const rollNumber = req.body.rollNumber;
-  const class = req.body.class;
-  const obtainedMarks = req.body.obtainedMarks;
-  const phoneNumber = req.body.phoneNumber;
-
-  // Save the student to the database
-  // ...
-
-  // Return a success response
-  res.status(201).json({ message: 'Student added successfully' });
-});
-
 // Get all students
-app.get('/students', async (req, res) => {
+async function getStudents() {
   // Get all students from the database
   // ...
 
   // Return the students
-  res.json(students);
-});
+  return students;
+}
 
-// Get a student by ID
-app.get('/students/:id', async (req, res) => {
-  const id = req.params.id;
-
-  // Get the student with the given ID from the database
+// Add a new student
+async function addStudent(name, rollNumber, class, obtainedMarks, phoneNumber) {
+  // Save the student to the database
   // ...
 
-  // If the student exists, return it
-  if (student) {
+  // Return the student
+  return student;
+}
+
+// Update a student
+async function updateStudent(id, name, rollNumber, class, obtainedMarks, phoneNumber) {
+  // Update the student in the database
+  // ...
+
+  // Return the updated student
+  return student;
+}
+
+// Delete a student
+async function deleteStudent(id) {
+  // Delete the student from the database
+  // ...
+}
+
+// Handle errors
+function handleError(error) {
+  console.log(error);
+
+  // Return an error response
+  return res.status(500).json({ message: 'An error occurred. Please try again later.' });
+}
+
+// Get all students
+app.get('/students', async (req, res) => {
+  try {
+    const students = await getStudents();
+
+    // Return the students
+    res.json(students);
+  } catch (error) {
+    handleError(error);
+  }
+});
+
+// Add a new student
+app.post('/add-student', async (req, res) => {
+  try {
+    const student = await addStudent(req.body.name, req.body.rollNumber, req.body.class, req.body.obtainedMarks, req.body.phoneNumber);
+
+    // Return the student
     res.json(student);
-  } else {
-    res.status(404).json({ message: 'Student not found' });
+  } catch (error) {
+    handleError(error);
+  }
+});
+
+// Update a student
+app.put('/students/:id', async (req, res) => {
+  try {
+    const student = await updateStudent(req.params.id, req.body.name, req.body.rollNumber, req.body.class, req.body.obtainedMarks, req.body.phoneNumber);
+
+    // Return the updated student
+    res.json(student);
+  } catch (error) {
+    handleError(error);
+  }
+});
+
+// Delete a student
+app.delete('/students/:id', async (req, res) => {
+  try {
+    await deleteStudent(req.params.id);
+
+    // Return a success response
+    res.status(200).json({ message: 'Student deleted successfully' });
+  } catch (error) {
+    handleError(error);
   }
 });
 
